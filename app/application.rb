c6a3fe5678bd5f92406456eb35634f9b9d0b902f
Isaac <izaroot327@gmail.com>
2021-06-02 16:40:57 -0400

    updated seed and req resp

--- app/application.rb
+++ app/application.rb
@@ -2,8 +2,12 @@ class Application
   def call(env)
     req = Rack::Request.new(env)
 
-    if req.path.match(/user=/)
+    if req.path.match(/user=/) && req.get?
       find_user(req.path.split('=').last.split('&'))
+    elsif req.path.match(/user=/) && req.post?
+      new_user(req.path.split('=').last, req)
+    elsif req.path.match(/tasks/) && req.get?
+      get_tasks
     else
       send_not_found
     end
@@ -26,5 +30,22 @@ class Application
     else 
       return [200, { "Content-Type" => "application/json" }, [{error: "Incorrect password"}.to_json]]
     end
+  end
+
+  def new_user(username, req)
+    result =  User.find_by(username: username)
+    if result 
+      return [200, { "Content-Type" => "application/json" }, [{error: "User already exists."}.to_json]]
+    else
+      user_hash = JSON.parse(req.body.read)
+      new_user = User.create(user_hash) 
+      return [201, { "Content-Type" => "application/json" }, [new_user.to_json(:include => {:user_tasks => {
+        :include => :task
+        }})]]
+    end
+  end
+
+  def get_tasks
+    return [200, { "Content-Type" => "application/json" }, [Task.all.to_json]]
   end
 end
