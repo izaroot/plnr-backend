bb2aee74e9436b3f3169b2eb4f806c045ec9a28c
Isaac <izaroot327@gmail.com>
2021-06-02 20:27:21 -0400

    added req response for new user task

--- app/application.rb
+++ app/application.rb
@@ -8,6 +8,8 @@ class Application
       new_user(req.path.split('=').last, req)
     elsif req.path.match(/tasks/) && req.get?
       get_tasks
+    elsif req.path.match(/newusertask/) && req.post?
+      new_user_task(req)
     else
       send_not_found
     end
@@ -38,11 +40,21 @@ class Application
       return [200, { "Content-Type" => "application/json" }, [{error: "User already exists."}.to_json]]
     else
       user_hash = JSON.parse(req.body.read)
+      binding.pry
       new_user = User.create(user_hash) 
       return [201, { "Content-Type" => "application/json" }, [new_user.to_json(:include => {:user_tasks => {
         :include => :task
         }})]]
     end
+  end
+
+  def new_user_task(req)
+    user_task_hash = JSON.parse(req.body.read)
+    user_task_hash["start_time"] = DateTime.parse(user_task_hash["start_time"]).strftime("%l,%P,%A,%U,%B,%d,%Y,%s")
+    user_task_hash["end_time"] = DateTime.parse(user_task_hash["end_time"]).strftime("%l,%P,%A,%U,%B,%d,%Y,%s")
+    binding.pry
+    new_user_task = UserTask.create(user_task_hash)
+    return [201, { "Content-Type" => "application/json" }, [new_user_task.to_json]]
   end
 
   def get_tasks
